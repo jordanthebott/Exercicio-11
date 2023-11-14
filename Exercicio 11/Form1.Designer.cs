9f937e83a7fc086d1ef6bbe3f5e38d230d475b0d
Tiago Jordão <122787916+jordanthebott@users.noreply.github.com>
2023-11-14 16:58:57 +0000

    Add project files.

---
+++ Exercicio 11/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Exercicio_11
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            menuStrip1 = new MenuStrip();
+            formatarToolStripMenuItem = new ToolStripMenuItem();
+            corETipoDeLetraToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(338, 188);
+            label1.Name = "label1";
+            label1.Size = new Size(70, 15);
+            label1.TabIndex = 1;
+            label1.Text = "POO em C#";
+            label1.Click += label1_Click;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { formatarToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 2;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // formatarToolStripMenuItem
+            // 
+            formatarToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { corETipoDeLetraToolStripMenuItem });
+            formatarToolStripMenuItem.Name = "formatarToolStripMenuItem";
+            formatarToolStripMenuItem.Size = new Size(67, 20);
+            formatarToolStripMenuItem.Text = "Formatar";
+            formatarToolStripMenuItem.Click += formatarToolStripMenuItem_Click;
+            // 
+            // corETipoDeLetraToolStripMenuItem
+            // 
+            corETipoDeLetraToolStripMenuItem.Name = "corETipoDeLetraToolStripMenuItem";
+            corETipoDeLetraToolStripMenuItem.Size = new Size(180, 22);
+            corETipoDeLetraToolStripMenuItem.Text = "Cor e tipo de letra";
+            corETipoDeLetraToolStripMenuItem.Click += corETipoDeLetraToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem formatarToolStripMenuItem;
+        private ToolStripMenuItem corETipoDeLetraToolStripMenuItem;
+    }
+}
---
+++ Exercicio 11/Form1.cs
@@ -0,0 +1,39 @@
+namespace Exercicio_11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void formatarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void corETipoDeLetraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                label1.Font = fontDialog.Font;
+            }
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                label1.ForeColor = colorDialog.Color;
+            }
+        }
+    }
+}
